0d079ff7c102ed8079513be016b3f7216253e125
OKayMeal <kaaaa765@gmail.com>
2025-01-06 16:38:52 +0100

    Glued together the entire setup and game logic in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,36 +1,47 @@
 #include <Arduino.h>
-#include <buzzer.h>
-#include <led.h>
-#include <button.h>
+#include <game_controller.h>
 
 Buzzer buzzer(3);
 LED led0(13);
 LED led1(12);
-Button btn0(2, 0, led0);
-Button btn1(4, 1, led1);
+LED led2(11);
+LED led3(10);
+LED failLed(5);
+LED successLed(4);
+Button btn0(9, 0, led0);
+Button btn1(8, 1, led1);
+Button btn2(7, 2, led2);
+Button btn3(6, 3, led3);
+bool initialized = false;
+Button* btns[MAX_BTNS] = { &btn0, &btn1, &btn2, &btn3 };
+GameController* gameController = nullptr;
 
 void setup()
 {
+	// Initialize serial com
 	Serial.begin(115200);
 	Serial.println("Serial communication established!");
 
+	// Initialize Buzzer, LEDs and Buttons
 	buzzer.begin();
 	led0.begin();
 	led1.begin();
-	btn0.begin();
-	btn1.begin();
-	btn0.setNote(&melody1[0]);
-	btn1.setNote(&melody1[1]);
+	led2.begin();
+	led3.begin();
+	failLed.begin();
+	successLed.begin();
+
+	for (int i = 0; i < MAX_BTNS; i++)
+	{
+		btns[i]->begin();
+	}
+
+	gameController = new GameController(btns, buzzer, successLed, failLed, melodies, MELODIES_COUNT);
 }
 
 void loop()
 {
-	// buzzer.playMelody(melody1, melody1Length);
-	// delay(3000);
-
-	if (btn0.isPressed()) {
-		btn0.handleBtnPress(buzzer);
-	} else if (btn1.isPressed()) {
-		btn1.handleBtnPress(buzzer);
+	if (gameController) {
+		gameController->update();
 	}
 }
